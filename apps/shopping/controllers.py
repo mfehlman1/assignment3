b42881d224602873d131d73496a31ab5fbd054d8
mfehlman1 <161093831+mfehlman1@users.noreply.github.com>
2024-11-22 10:43:20 -0800

    Update controllers.py

--- apps/shopping/controllers.py
+++ apps/shopping/controllers.py
@@ -29,7 +29,7 @@ from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from py4web.utils.url_signer import URLSigner
-from .models import get_user_emailS
+from .models import get_user_email
 
 url_signer = URLSigner(session)
 
